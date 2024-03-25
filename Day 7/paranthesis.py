8031ca233f048fafb7c3e8ddc2b2f490bc51a0d6
Girinan <girinandhu0507@gmail.com>
2024-03-25 22:23:22 +0530

    new

--- Day 7/paranthesis.py
+++ Day 7/paranthesis.py
@@ -1,13 +1,14 @@
-user_inp = list(input("Enter the string : ").split(" "))
+# paranathesis using stack algorithm 
+user_inp = list(input("Enter the string : ").split(" "))# getting multiple inputs and sep using " "
 for i in user_inp:
     stack = []
-    count = 0
+    flag = 0
     for j in i:
         if j == "(":
-            stack.append(j)
-        elif len(stack)==0 and j == ")":
-            count+=1
-        elif j == ")":
+            stack.append(j)# if j is equal to open bracket append it to stack list
+        elif len(stack)==0 and j == ")":# if we have close bracket in starting of stack increment flag
+            flag+=1
+        elif j == ")":# perform pop when j is equal to close bracket 
             stack.pop()
-    if len(stack) == 0 and count == 0:
+    if len(stack) == 0 and flag == 0:# print i if it is valid input 
         print(i)
