949d8c7424d300bb27719d688b35608e320112df
Girinan <girinandhu0507@gmail.com>
2024-03-27 20:22:48 +0530

    new

--- Day 7/paranthesis.py
+++ Day 7/paranthesis.py
@@ -2,13 +2,16 @@
 user_inp = list(input("Enter the string : ").split(" "))# getting multiple inputs and sep using " "
 for i in user_inp:
     stack = []
-    flag = 0
+    copy = ""
     for j in i:
         if j == "(":
             stack.append(j)# if j is equal to open bracket append it to stack list
         elif len(stack)==0 and j == ")":# if we have close bracket in starting of stack increment flag
-            flag+=1
+            break
         elif j == ")":# perform pop when j is equal to close bracket 
             stack.pop()
-    if len(stack) == 0 and flag == 0:# print i if it is valid input 
-        print(i)
+        copy = i
+    if copy != "":# print copy if it is valid input 
+        print(copy)
+    else:
+        print("No valid input is given ")
