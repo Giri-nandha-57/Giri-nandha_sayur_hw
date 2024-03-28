544c8996302ff71a5965a711e30ee35a9ba7fead
Girinan <girinandhu0507@gmail.com>
2024-03-28 14:25:43 +0530

    updated the last homework

---
+++ Day 7/all_brackets.py
@@ -0,0 +1,26 @@
+# paranathesis using stack algorithm 
+user_inp = list(input("Enter the string : ").split(" "))# getting multiple inputs and sep using " "
+
+def check_last_element(stack):
+    return stack[-1]# return the last element is the stack list
+
+for i in user_inp:
+    stack = []
+    for j in i:
+        copy = ""
+        if j == "(" or j == "{" or j == "[":
+            stack.append(j)# if j is equal to any of the open bracket append it to stack list
+        elif len(stack)==0 and (j == ")" or j == "}" or j == "]"):# if we have any of the close bracket in starting of stack increment flag
+            break
+        elif j == ")" or j == "}" or j == "]":
+            if j == "]" and check_last_element(stack)== "[" :# check for the respective open bracket in stack
+                stack.pop()
+            if j == "}" and check_last_element(stack)== "{" :# check for the respective open bracket in stack
+                stack.pop()
+            if j == ")" and check_last_element(stack)== "(" :# check for the respective open bracket in stack
+                stack.pop()
+        copy = i
+    if len(stack) == 0 and copy !="":# print i if it is valid input 
+        print(i)
+    else:
+        print("not a valid input ")
